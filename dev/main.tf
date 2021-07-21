ade3fd0aba4e071ee357b37e13e6f6a664bc57fc
aldoranx <geraldwork1@gmail.com>
2021-07-21 11:15:54 +0200

    vpc/ec2 setup v1

---
+++ dev/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+module "my_vpc" {
+  source      = "../modules/vpc"
+  vpc_cidr    = "10.0.0.0/16"
+  tenancy     = "default"
+  vpc_id      = "${module.my_vpc.vpc_id}"
+  subnet_cidr = "10.0.1.0/24"
+}
+
+module "my_ec2" {
+  source        = "../modules/ec2"
+  ec2_count     = 1
+  ami_id        = "ami-0f7cd40eac2214b37"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.my_vpc.subnet_id}"
+}
